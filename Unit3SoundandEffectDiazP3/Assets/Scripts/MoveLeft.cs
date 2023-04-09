24578f0c761719da28ea28eaaf4b76646da0b0ae
Daniel Diaz <710147@student.dusd.net>
2023-04-08 20:42:49 -0700

    finished challenge 3 and bonus
    finished challenge 3 and bonus

--- Unit3SoundandEffectDiazP3/Assets/Scripts/MoveLeft.cs
+++ Unit3SoundandEffectDiazP3/Assets/Scripts/MoveLeft.cs
@@ -17,7 +17,11 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerControllerScript.gameOver == false)
+        if (playerControllerScript.doubleSpeed)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * (speed * 2));
+        }
+        else
         {
             transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
